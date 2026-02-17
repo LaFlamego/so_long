376b638e7f9eaf0a28570d3941872b07cec0adee
Yueyuan Li <yueli@f4r5s16.clusters.42paris.fr>
2026-02-17 18:19:55 +0100

    map: valid tile, enclosed; to do valid path

--- lib/libft/libft.h
+++ lib/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: yueli <yueli@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 15:08:10 by yueli             #+#    #+#             */
-/*   Updated: 2026/02/13 15:07:31 by yueli            ###   ########.fr       */
+/*   Updated: 2026/02/17 17:56:12 by yueli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_calloc(size_t n, size_t size);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *str);
+size_t	ft_strnb(char **strs);
 size_t	ft_strlcpy(char *s1, const char *s2, size_t dsize);
 size_t	ft_strlcat(char *s1, const char *s2, size_t dsize);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
--- src/map_check.c
+++ src/map_check.c
@@ -6,7 +6,7 @@
 /*   By: yueli <yueli@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/01 15:35:05 by yueli             #+#    #+#             */
-/*   Updated: 2026/02/16 12:13:11 by yueli            ###   ########.fr       */
+/*   Updated: 2026/02/17 18:05:30 by yueli            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,89 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-//if rectangular
-//if 01CEP(only)
-//if P=1, E=1, C>=1
+size_t	ft_strlen(const char *str);
+size_t	ft_strnb(char **strs);
+
+//if rectangular (done)
+//if 01CEP(only) (done)
+//if P=1, E=1, C>=1 (done)
 //if enclosed by 1
 //if valid path(at least one way to go)
 
-// bool    is_rectangular(char **map)
-// {
-//     int line_len;
+bool    is_rectangular(char **map)
+{
+    int line_len;
     
-//     if (!map || !*map)
-//         return (false);
-//     line_len = ft_strlen(*map);
-//     while (*map)
-//     {
-//         if (ft_strlen(*map) != line_len)
-//             return (false);
-//         ++map;
-//     }
-//     return (true);
-// }
+    if (!map || !*map)
+        return (false);
+    line_len = ft_strlen(*map);
+    while (*map)
+    {
+        if (ft_strlen(*map) != line_len)
+            return (false);
+        ++map;
+    }
+    return (true);
+}
 
 bool    is_valid_char(char **map)
 {
     int i;
-    int j;
+    int lookup[3] = {};
 
-    j = 0;
-    while (map[j])
+    while (*map)
     {
         i = 0;
-        while (map[j][i])
+        while ((*map)[i])
         {
-            if (map[j][i] != '0' && map[j][i] != '1' && map[j][i] != 'C'
-                && map[j][i]!= 'E' && map[j][i] != 'P')
+            if ((*map)[i] != '0' && (*map)[i] != '1' && (*map)[i] != 'C'
+                && (*map)[i]!= 'E' && (*map)[i] != 'P')
                 return (false);
+            if ((*map)[i] == 'P')
+                lookup[0] += 1;
+            else if ((*map)[i] == 'E')
+                lookup[1] += 1;
+            else if ((*map)[i] == 'C')
+                lookup[2] += 1;
             ++i;
         }
-        ++j;
+        ++map;
+    }
+    if (lookup[0] != 1 || lookup[1] != 1 || lookup[2] < 1)
+        return (false);
+    return (true);
+}
+
+bool    is_all_one(char *line)
+{
+    while (*line)
+    {
+        if (*line != '1')
+            return (false);
+        ++line;
+    }
+    return (true);
+}
+
+bool    is_enclosed(char **map)
+{
+    int     i;
+    size_t  line_nb;
+    size_t  line_len;
+
+    i = 1;
+    line_nb = ft_strnb(map);
+    line_len = ft_strlen(*map);
+    printf("1111\n");
+    if (!is_all_one(map[0]) || !is_all_one(map[line_nb - 1]))
+        return (false);
+    printf("2222\n");
+    while (i < line_nb - 1)
+    {
+        printf("%s\n", map[i]);
+        if (map[i][0] != '1' || map[i][line_len - 1] != '1')
+            return (false);
+        ++i;
     }
     return (true);
 }
@@ -64,8 +108,8 @@ bool    is_valid_char(char **map)
 
 int main(void)
 {
-    char *map[] = {"11111", NULL};
+    char *map[] = {"11111", "10001", "10001", "10001", "11111", NULL};
 
-    printf("%d\n", is_valid_char(map));
+    printf("%d\n", is_enclosed(map));
     return 0;
 }
